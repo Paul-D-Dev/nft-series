59237c7f9eb0c4b53d183856eda79c81fca10c6d
Paul DAO <dao.van.paul@gmail.com>
2023-07-06 17:27:27 -0400

    FEAT implement theme-toggle component into nav bar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, ThemeToggleComponent],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
