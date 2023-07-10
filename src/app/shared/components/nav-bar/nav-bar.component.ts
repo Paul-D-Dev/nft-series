b839cd7b3e4cadba0d6caf12167be8ac783a2ee7
Paul DAO <dao.van.paul@gmail.com>
2023-07-10 12:13:39 -0400

    FEAT nav bar toggle sideNav

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Icons } from '../../enums';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, ThemeToggleComponent],
+  imports: [CommonModule, MatToolbarModule, ThemeToggleComponent, MatIconModule, MatButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  protected readonly Icons = Icons;
+
+  @Output() toggleSideNav = new EventEmitter();
 
 }
