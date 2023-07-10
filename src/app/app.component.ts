eca75cfa860d583136295b7ad8ea61fbc9eefd98
Paul DAO <dao.van.paul@gmail.com>
2023-07-10 12:13:27 -0400

    FEAT app implement side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
