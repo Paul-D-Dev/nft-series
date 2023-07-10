3d524f6615d6e9740aadb1221961a2d7cb4c097a
Paul DAO <dao.van.paul@gmail.com>
2023-07-10 13:02:38 -0400

    FEAT app add in sidenav theme toggle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './shared/components';
+import { NavBarComponent, ThemeToggleComponent } from './shared/components';
+import { Icons } from './shared/enums';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, MatSidenavModule],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, MatSidenavModule, MatListModule, MatIconModule, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nft-series';
+  protected readonly Icons = Icons;
 }
