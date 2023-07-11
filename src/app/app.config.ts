dc348668a00e314a8edf2b6d347653587202c7ed
Paul DAO <dao.van.paul@gmail.com>
2023-07-11 12:40:31 -0400

    FEAT app config swiper register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { register } from 'swiper/element/bundle';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations()]
 };
+
+register();
+
