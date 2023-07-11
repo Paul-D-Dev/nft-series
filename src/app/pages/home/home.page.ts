4ca29013e250fc1ee514a7faceb1916fd6e0c92d
Paul DAO <dao.van.paul@gmail.com>
2023-07-11 12:41:38 -0400

    FEAT Home page use swiper to display list card link

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { CardLinkComponent } from '../../shared/components';
+import { CardLinkComponent, SwiperComponent } from '../../shared/components';
+import { CardLink } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardLinkComponent],
+  imports: [CommonModule, CardLinkComponent, SwiperComponent],
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
-
+  cards: CardLink[] = [
+    {title: 'Stranger Things', floor: 0.09, devise: 'ETH', imgUrl: 'https://picsum.photos/600'},
+    {title: 'Stranger Things 2', floor: 1.35, devise: 'BSC', imgUrl: 'https://picsum.photos/600'},
+    {title: 'Stranger Things 3', floor: 10.59, devise: 'POL', imgUrl: 'https://picsum.photos/600'},
+  ]
 }
