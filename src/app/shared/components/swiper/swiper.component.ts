9c21898199712365a3cd7750133d6b4f04df22b5
Paul DAO <dao.van.paul@gmail.com>
2023-07-12 15:03:03 -0400

    FEAT implement swiper component

--- src/app/shared/components/swiper/swiper.component.ts
+++ src/app/shared/components/swiper/swiper.component.ts
@@ -1,15 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, TemplateRef, ContentChild } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
+import { SwiperDirective } from '../../directives/swiper.directive';
 
 @Component({
   selector: 'app-swiper',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SwiperDirective],
   templateUrl: './swiper.component.html',
   styleUrls: ['./swiper.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SwiperComponent {
   @ContentChild('templateSlide') templateSlide!: TemplateRef<any>;
+  @Input() config: SwiperOptions = {
+    navigation: true,
+    spaceBetween: 16,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        spaceBetween: 50,
+      },
+    }
+  }
   @Input() sliders: unknown[] = [];
+
 }
