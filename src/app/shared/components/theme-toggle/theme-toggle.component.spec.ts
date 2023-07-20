9bcfa9d1f90f53ffaa47bac072b615d08b348d26
Paul DAO <dao.van.paul@gmail.com>
2023-07-20 15:27:57 -0400

    WIP TEST theme toggle component display the right icon

--- src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
@@ -2,8 +2,10 @@ import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatIconHarness } from '@angular/material/icon/testing';
 import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
 import { of, Observable } from 'rxjs';
+import { Icons } from '../../enums';
 import { ThemeService } from '../../services/theme.service';
 import { ThemeToggleComponent } from './theme-toggle.component';
 
@@ -60,6 +62,16 @@ describe('ThemeToggleComponent', () => {
     component.isDarkTheme$ = of(false);
     fixture.detectChanges();
     expect(await slideToggle.isChecked()).toBe(false);
+  })
+
+  it('should display the right icon', async () => {
+    const icon = await loader.getHarness(MatIconHarness);
+    expect(await icon.getName()).toBe(Icons.DarkMode);
+
+    component.isDarkTheme$ = of(false);
+    fixture.detectChanges();
+    expect(await icon.getName()).toBe(Icons.LightMode);
+
   })
 
 });
