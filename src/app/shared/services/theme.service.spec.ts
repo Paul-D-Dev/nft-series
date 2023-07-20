2169648e9748965606d541b243ca8411e7a043cb
Paul DAO <dao.van.paul@gmail.com>
2023-07-20 14:12:46 -0400

    WIP test theme service

--- src/app/shared/services/theme.service.spec.ts
+++ src/app/shared/services/theme.service.spec.ts
@@ -1,16 +1,24 @@
+import { Renderer2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
 
 describe('ThemeService', () => {
   let service: ThemeService;
+  const mockRenderer = jasmine.createSpyObj(['addClass', 'removeClass'])
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ThemeService,
+        {provide: Renderer2, useValue: mockRenderer}
+      ]
+    });
     service = TestBed.inject(ThemeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
