c27eff3cfe78e89b1ec2b46a5e99d119f6993867
Paul DAO <dao.van.paul@gmail.com>
2023-07-24 13:13:16 -0400

    WIP theme test onChangeSlide call once fn and fn with value

--- src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.spec.ts
@@ -1,10 +1,10 @@
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconHarness } from '@angular/material/icon/testing';
 import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
-import { of, Observable } from 'rxjs';
+import { of } from 'rxjs';
 import { Icons } from '../../enums';
 import { ThemeService } from '../../services/theme.service';
 import { ThemeToggleComponent } from './theme-toggle.component';
@@ -15,18 +15,11 @@ describe('ThemeToggleComponent', () => {
   let fixture: ComponentFixture<ThemeToggleComponent>;
   let loader: HarnessLoader;
 
-  let themeServiceSpy = jasmine.createSpyObj('ThemeService', ['userThemeIsDark$']);
+  let themeServiceSpy = jasmine.createSpyObj('ThemeService', ['userThemeIsDark$', 'toggleUserTheme']);
   let themeService: ThemeService;
-  let isDark: boolean;
-  let userThemeIsDarkSpy$: Observable<boolean>;
   let el: DebugElement;
 
   beforeEach(() => {
-    // isDark = true;
-    userThemeIsDarkSpy$ = themeServiceSpy.userThemeIsDark$.and.returnValue(of(isDark));
-    waitForAsync(() => {
-
-    })
     TestBed.configureTestingModule({
       imports: [ThemeToggleComponent],
       providers: [{provide: ThemeService, useValue: themeServiceSpy}]
@@ -37,7 +30,7 @@ describe('ThemeToggleComponent', () => {
     fixture.detectChanges();
     el = fixture.debugElement;
 
-    themeService = TestBed.inject(ThemeService);
+    themeService = TestBed.inject<ThemeService>(ThemeService);
   });
 
   it('should create', () => {
@@ -58,7 +51,6 @@ describe('ThemeToggleComponent', () => {
     component.isDarkTheme$ = of(true);
     fixture.detectChanges();
     expect(await slideToggle.isChecked()).toBe(true);
-
     component.isDarkTheme$ = of(false);
     fixture.detectChanges();
     expect(await slideToggle.isChecked()).toBe(false);
@@ -71,7 +63,14 @@ describe('ThemeToggleComponent', () => {
     component.isDarkTheme$ = of(false);
     fixture.detectChanges();
     expect(await icon.getName()).toBe(Icons.LightMode);
+  })
 
+  it('should call toggleTheme on mat-slide-toggle change', async () => {
+    spyOn(component, 'toggleTheme');
+    const slideToggle = await loader.getHarness(MatSlideToggleHarness);
+    await slideToggle.toggle();
+    expect(component.toggleTheme).toHaveBeenCalledTimes(1);
+    expect(component.toggleTheme).toHaveBeenCalledWith(false);
   })
 
 });
