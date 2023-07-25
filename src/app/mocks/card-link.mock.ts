f754eba547e9cb22ef0609800468b324242d53e4
Paul DAO <dao.van.paul@gmail.com>
2023-07-25 15:10:52 -0400

    FEAT create mock card link

---
+++ src/app/mocks/card-link.mock.ts
@@ -0,0 +1,52 @@
+import { CardLink } from '../shared/interfaces';
+
+export const cardLinkMock: CardLink[] = [
+  {
+    id: 1,
+    nameSeo: 'stranger-things',
+    title: 'Stranger Things',
+    floor: 0.09,
+    devise: 'ETH',
+    imgUrl: 'https://picsum.photos/600'
+  },
+  {
+    id: 2,
+    nameSeo: 'stranger-things-1',
+    title: 'Stranger Things 2',
+    floor: 1.35,
+    devise: 'BSC',
+    imgUrl: 'https://picsum.photos/600'
+  },
+  {
+    id: 3,
+    nameSeo: 'stranger-things-2',
+    title: 'Stranger Things 3',
+    floor: 10.59,
+    devise: 'POL',
+    imgUrl: 'https://picsum.photos/600'
+  },
+  {
+    id: 4,
+    nameSeo: 'stranger-things-3',
+    title: 'Stranger Things',
+    floor: 0.09,
+    devise: 'ETH',
+    imgUrl: 'https://picsum.photos/600'
+  },
+  {
+    id: 5,
+    nameSeo: 'stranger-things-4',
+    title: 'Stranger Things 2',
+    floor: 1.35,
+    devise: 'BSC',
+    imgUrl: 'https://picsum.photos/600'
+  },
+  {
+    id: 6,
+    nameSeo: 'stranger-things-5',
+    title: 'Stranger Things 3',
+    floor: 10.59,
+    devise: 'POL',
+    imgUrl: 'https://picsum.photos/600'
+  },
+];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { cardLinkMock } from '../../mocks';
 import { CardLinkComponent, SwiperComponent } from '../../shared/components';
 import { CardLink } from '../../shared/interfaces';
 
@@ -11,12 +12,5 @@ import { CardLink } from '../../shared/interfaces';
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
-  cards: CardLink[] = [
-    {title: 'Stranger Things', floor: 0.09, devise: 'ETH', imgUrl: 'https://picsum.photos/600'},
-    {title: 'Stranger Things 2', floor: 1.35, devise: 'BSC', imgUrl: 'https://picsum.photos/600'},
-    {title: 'Stranger Things 3', floor: 10.59, devise: 'POL', imgUrl: 'https://picsum.photos/600'},
-    {title: 'Stranger Things', floor: 0.09, devise: 'ETH', imgUrl: 'https://picsum.photos/600'},
-    {title: 'Stranger Things 2', floor: 1.35, devise: 'BSC', imgUrl: 'https://picsum.photos/600'},
-    {title: 'Stranger Things 3', floor: 10.59, devise: 'POL', imgUrl: 'https://picsum.photos/600'},
-  ]
+  cards: CardLink[] = cardLinkMock;
 }
