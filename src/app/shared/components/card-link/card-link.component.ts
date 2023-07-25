e725ab0f6d05198d5a3e094ee7556b4a326c3c06
Paul DAO <dao.van.paul@gmail.com>
2023-07-25 15:12:03 -0400

    FEAT card link component remove a and add router link to collection + name seo

--- src/app/shared/components/card-link/card-link.component.ts
+++ src/app/shared/components/card-link/card-link.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 import { CardLink } from '../../interfaces';
 
 @Component({
   selector: 'app-card-link',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, RouterLink],
   templateUrl: './card-link.component.html',
   styleUrls: ['./card-link.component.scss']
 })
