f88ecf14bd8a6193a15f92594f7f2000456b46f8
Paul DAO <dao.van.paul@gmail.com>
2023-07-25 15:11:25 -0400

    FEAT generate collection detail page

---
+++ src/app/pages/collection-detail/collection-detail.page.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CollectionDetailPage } from './collection-detail.page';
+
+describe('CollectionDetailPage', () => {
+  let component: CollectionDetailPage;
+  let fixture: ComponentFixture<CollectionDetailPage>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CollectionDetailPage]
+    });
+    fixture = TestBed.createComponent(CollectionDetailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/collection-detail/collection-detail.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-collection-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './collection-detail.page.html',
+  styleUrls: ['./collection-detail.page.scss']
+})
+export class CollectionDetailPage {
+
+}
