a361a40229d381032d862e5fb2ca4e97a3a40911
Paul DAO <dao.van.paul@gmail.com>
2023-07-25 15:11:36 -0400

    Update app route with new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { CollectionDetailPage } from './pages/collection-detail/collection-detail.page';
 import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'collections/:collectionName',
+    component: CollectionDetailPage
   }
 ];
