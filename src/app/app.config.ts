34ef091110091a2a1cd198f02555da118dfd3eeb
Paul DAO <dao.van.paul@gmail.com>
2023-07-25 16:05:06 -0400

    FEAT config add withComponentInputBinding to read params route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()]
 };
 
 register();
