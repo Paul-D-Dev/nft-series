76131b91226476bc1bc71281c34c35d7d91c1361
Paul DAO <dao.van.paul@gmail.com>
2023-07-27 17:36:04 -0400

    FEAT Collection detail casting tab

--- src/app/pages/collection-detail/collection-detail.page.ts
+++ src/app/pages/collection-detail/collection-detail.page.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Icons } from '../../shared/enums';
@@ -13,13 +14,19 @@ interface CollectionDetail {
   description: string;
   media: {
     shareUrl: string;
-  }
+  },
+  casting: Casting[]
+}
+
+interface Casting {
+  actor: string,
+  character: string
 }
 
 @Component({
   selector: 'app-collection-detail',
   standalone: true,
-  imports: [CommonModule, MatTabsModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatTabsModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './collection-detail.page.html',
   styleUrls: ['./collection-detail.page.scss']
 })
@@ -38,7 +45,21 @@ export class CollectionDetailPage {
     description: '"Freaks" (1932), directed by Tod Browning, is a cult horror film set within a traveling circus troupe comprised of individuals with physical deformities, affectionately known as "freaks." When a dwarf named Hans falls in love with the beautiful trapeze artist Cleopatra, she manipulates his emotions to inherit his fortune. However, the other members of the troupe uncover her scheme and seek revenge. The film explores themes of tolerance, family, and vengeance, and has become a classic for its originality and controversy.',
     media: {
       shareUrl: '#'
-    }
+    },
+    casting: [
+      {
+        actor: 'Wallace Ford',
+        character: 'Hans'
+      },
+      {
+        actor: 'Leila Hyams',
+        character: 'Venus'
+      },
+      {
+        actor: 'Olga Baclanova',
+        character: 'Cleopatra'
+      }
+    ]
   };
 
 }
