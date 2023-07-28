f0b6f73cf749caebfe817e401b1c07de065c2693
Paul DAO <dao.van.paul@gmail.com>
2023-07-28 17:02:00 -0400

    UPDATE Icons add filter and swap

--- src/app/shared/enums/icons.enum.ts
+++ src/app/shared/enums/icons.enum.ts
@@ -1,9 +1,11 @@
 export enum Icons {
   Close = 'close',
   DarkMode = 'dark_mode',
+  FilterList = 'filter_list',
   LightMode = 'light_mode',
   Menu = 'menu',
   MoreHoriz = 'more_horiz',
   Share = 'share',
+  SwapVert = 'swap_vert',
   Wallet = 'wallet',
 }
