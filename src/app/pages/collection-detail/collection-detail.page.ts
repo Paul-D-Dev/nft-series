f5aa4e2594e4df2f0fd72044b64abd05f6f99cae
Paul DAO <dao.van.paul@gmail.com>
2023-10-18 15:55:25 -0400

    FEAT collection detail filters ui

--- src/app/pages/collection-detail/collection-detail.page.ts
+++ src/app/pages/collection-detail/collection-detail.page.ts
@@ -4,9 +4,17 @@ import { Component, Input } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
+
+
 import { Icons } from '../../shared/enums';
 
 interface CollectionDetail {
@@ -29,7 +37,7 @@ interface Casting {
 @Component({
   selector: 'app-collection-detail',
   standalone: true,
-  imports: [CommonModule, MatTabsModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule],
+  imports: [CommonModule, MatTabsModule, MatButtonModule, MatIconModule, MatCardModule, MatSidenavModule, MatExpansionModule, MatChipsModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatCheckboxModule],
   templateUrl: './collection-detail.page.html',
   styleUrls: ['./collection-detail.page.scss']
 })
@@ -37,7 +45,7 @@ export class CollectionDetailPage {
   constructor(breakpointObserver: BreakpointObserver) {
     breakpointObserver.observe('(min-width: 967px)').pipe(
       takeUntilDestroyed()
-    ).subscribe(bp => this.isDesktop = bp.matches)
+    ).subscribe(bp => this.isOpenedFilters = bp.matches)
   }
 
   @Input()
@@ -46,7 +54,7 @@ export class CollectionDetailPage {
   }
 
   protected readonly Icons = Icons;
-  isDesktop!: boolean;
+  isOpenedFilters: boolean = false;
   collectionDetail: CollectionDetail | null = {
     imgAvatar: 'https://kultt.fr/wp-content/uploads/2022/02/poster-freaks_02.jpg',
     imgBanner: 'https://kultt.fr/wp-content/uploads/2022/02/poster-freaks_02.jpg',
@@ -71,5 +79,9 @@ export class CollectionDetailPage {
       }
     ]
   };
+
+  toggleFilters(): void {
+    this.isOpenedFilters = !this.isOpenedFilters;
+  }
 
 }
