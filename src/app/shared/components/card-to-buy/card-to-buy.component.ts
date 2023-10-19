6b6d676b62b8b56f281b379d2f363096ba807e6a
Paul DAO <dao.van.paul@gmail.com>
2023-10-19 12:10:47 -0400

    CHORES card to buy: add TODO

--- src/app/shared/components/card-to-buy/card-to-buy.component.ts
+++ src/app/shared/components/card-to-buy/card-to-buy.component.ts
@@ -5,12 +5,13 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-    selector: 'app-card-to-buy',
-    standalone: true,
-    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
-    templateUrl: './card-to-buy.component.html',
-    styleUrls: ['./card-to-buy.component.scss']
+  selector: 'app-card-to-buy',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  templateUrl: './card-to-buy.component.html',
+  styleUrls: ['./card-to-buy.component.scss']
 })
 export class CardToBuyComponent {
+//     TODO: inputs
 
 }
