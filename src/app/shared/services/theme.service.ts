c806614f00be91008daa9315d0ad2dc94ff5626f
Paul DAO <dao.van.paul@gmail.com>
2023-11-06 13:56:08 -0500

    UPDATE theme service private

--- src/app/shared/services/theme.service.ts
+++ src/app/shared/services/theme.service.ts
@@ -1,45 +1,48 @@
-import { DOCUMENT } from '@angular/common';
-import { Injectable, inject, Renderer2 } from '@angular/core';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { inject, Injectable, Renderer2 } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Theme } from '../enums';
+import { DOCUMENT } from "@angular/common";
 
 @Injectable()
 export class ThemeService {
+
   private _document: Document = inject(DOCUMENT);
   private _renderer: Renderer2 = inject(Renderer2);
-  private readonly _darkPrefersColorScheme: boolean = window.matchMedia(`(prefers-color-scheme: ${Theme.Dark})`).matches;
-  private _userTheme$: BehaviorSubject<Theme | null> = new BehaviorSubject<Theme | null>(null);
-  private get _userTheme(): Theme | null {
-    return this._userTheme$.getValue();
+  readonly darkPrefersColorScheme: boolean = window.matchMedia(`(prefers-color-scheme: ${Theme.Dark})`).matches;
+  userTheme$: BehaviorSubject<Theme | null> = new BehaviorSubject<Theme | null>(null);
+
+  get userTheme(): Theme | null {
+    return this.userTheme$.getValue();
   }
+
   get userThemeIsDark$(): Observable<boolean> {
-    return this._userTheme$.asObservable().pipe(
+    return this.userTheme$.asObservable().pipe(
       map((theme: Theme | null): boolean => theme === Theme.Dark)
     );
   }
 
   initUserTheme(): void {
-    if (!this._userTheme) {
-      this.toggleUserTheme(this._darkPrefersColorScheme);
+    if (!this.userTheme) {
+      this.toggleUserTheme(this.darkPrefersColorScheme);
     } else {
-      this.toggleUserTheme(this._userTheme === Theme.Dark);
+      this.toggleUserTheme(this.userTheme === Theme.Dark);
     }
   }
 
   toggleUserTheme(bool: boolean): void {
-    this._userTheme$.next(bool ? Theme.Dark : Theme.Light);
+    this.userTheme$.next(bool ? Theme.Dark : Theme.Light);
     if (bool) {
-      this._setDarkTheme()
+      this.setDarkTheme()
     } else {
-      this._removeDarkTheme();
+      this.removeDarkTheme();
     }
   }
 
-  private _setDarkTheme(): void {
+  setDarkTheme(): void {
     this._renderer.addClass(this._document.body, `${Theme.Dark}-theme`);
   }
 
-  private _removeDarkTheme(): void {
+  removeDarkTheme(): void {
     this._renderer.removeClass(this._document.body, `${Theme.Dark}-theme`);
   }
 }
