f83c701b1db30686945210be08c9b1a422f428f0
Paul DAO <dao.van.paul@gmail.com>
2023-11-06 14:18:53 -0500

    UPDATE nav bar inject router and uses navigateByUrl

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,21 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { Icons } from '../../enums';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
   protected readonly Icons = Icons;
+  _router = inject(Router);
   @Input() isActiveSideNav: boolean = false
   @Output() toggleSideNav = new EventEmitter();
+
+  navigateToHomePage(): void {
+    this._router.navigateByUrl('/');
+  }
 
 }
