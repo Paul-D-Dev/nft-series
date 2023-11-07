a088f379a66259eeba9a887ab87751e1cf731fb5
Paul DAO <dao.van.paul@gmail.com>
2023-11-07 14:24:49 -0500

    WIP nav bar connect wallet with metamask

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,12 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule }                                   from '@angular/common';
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router } from '@angular/router';
-import { Icons } from '../../enums';
+import { MatButtonModule }                                from '@angular/material/button';
+import { MatIconModule }                                  from '@angular/material/icon';
+import { MatToolbarModule }                               from '@angular/material/toolbar';
+import { Router }                                         from '@angular/router';
+import { Observable }                                     from 'rxjs';
+import { Icons }                                          from '../../enums';
+import { AuthService }                                    from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,8 +16,10 @@ import { Icons } from '../../enums';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  protected readonly Icons = Icons;
   _router: Router = inject(Router);
+  private authService: AuthService = inject(AuthService);
+  protected readonly Icons = Icons;
+  readonly metaAddress$: Observable<string> = this.authService.metaAddress$;
   @Input() isActiveSideNav: boolean = false
   @Output() toggleSideNav = new EventEmitter();
 
@@ -25,6 +29,17 @@ export class NavBarComponent {
 
   emitToggleSideNav(): void {
     this.toggleSideNav.emit();
+  }
+
+  connectToMetaMask() {
+    this.authService.signInWithMetaMask().subscribe({
+      next(data) {
+        console.log(data)
+      },
+      error(err) {
+        console.error(err);
+      }
+    });
   }
 
 }
