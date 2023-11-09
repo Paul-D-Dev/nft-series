57299c87d62616e299ffc523ea7cbe9127d7493d
Paul DAO <dao.van.paul@gmail.com>
2023-11-09 15:03:55 -0500

    FEAT cart component animation

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,31 +1,59 @@
-import { DialogRef }        from '@angular/cdk/dialog';
-import { CommonModule }     from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { MatButtonModule }  from '@angular/material/button';
-import { MatIconModule }    from '@angular/material/icon';
-import { MatListModule }    from '@angular/material/list';
-import { Icons }            from '../../enums';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { DialogRef }                           from '@angular/cdk/dialog';
+import { CommonModule }                        from '@angular/common';
+import { Component, Input }                    from '@angular/core';
+import { MatButtonModule }                     from '@angular/material/button';
+import { MatIconModule }                       from '@angular/material/icon';
+import { MatListModule }                       from '@angular/material/list';
+import { Icons }                               from '../../enums';
+
+const slideInOutAnimationTime = 150;
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [CommonModule, MatIconModule, MatButtonModule, MatListModule],
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate(slideInOutAnimationTime, style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate(slideInOutAnimationTime, style({ transform: 'translateX(100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class CartComponent {
-  constructor(public dialogRef: DialogRef) {}
+  constructor(public dialogRef: DialogRef) {
+    this.isOpened = true;
+    this.dialogRef.disableClose = true;
+    this.dialogRef.backdropClick.subscribe(() => {
+      this._closeDialogWithAnimation();
+    })
+  }
 
-  @Input() items: unknown[] = [];
+  protected readonly Icons = Icons;
+  isOpened = true;
   isDisabled: boolean = false;
 
+  @Input() items: unknown[] = [];
+
   closeCart(): void {
-    console.log('close cart');
-    this.dialogRef.close();
+    this._closeDialogWithAnimation();
   }
 
   clearCart(): void {
     console.log('clear cart');
   }
-  protected readonly Icons = Icons;
+
+  private _closeDialogWithAnimation() {
+    this.isOpened = false;
+    setTimeout(() => {
+      this.dialogRef.close()
+    }, slideInOutAnimationTime)
+  }
 }
