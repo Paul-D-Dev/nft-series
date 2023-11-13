8ade807f3a24ea9f31a90e719e55a15ff6b77dfe
Paul DAO <dao.van.paul@gmail.com>
2023-11-13 11:48:52 -0500

    UPDATE cart to buy interface: add id

--- src/app/mocks/card-to-buy.mock.ts
+++ src/app/mocks/card-to-buy.mock.ts
@@ -1,6 +1,7 @@
-import { CardToBuy } from "../shared/interfaces";
+import { CardToBuy } from '../shared/interfaces';
 
 export const cardToBuyMock: CardToBuy = {
+  id: '1231',
   idListing: 332,
   title: 'Chaos Road',
   price: 0.99,
--- src/app/shared/interfaces/card-to-buy.interface.ts
+++ src/app/shared/interfaces/card-to-buy.interface.ts
@@ -1,4 +1,5 @@
 export interface CardToBuy {
+  id: string,
   idListing: number,
   title: string,
   price: number,
