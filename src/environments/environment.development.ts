4b455512dc37dee6abaa129dc0a8e37e54379e43
Paul DAO <dao.van.paul@gmail.com>
2023-11-14 14:21:55 -0500

    FEAT env with firebase config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyD2SmZU5N6wjr-EES2FhMrXirhQB_Kcef8",
+    authDomain: "ntf-series.firebaseapp.com",
+    projectId: "ntf-series",
+    storageBucket: "ntf-series.appspot.com",
+    messagingSenderId: "963615637081",
+    appId: "1:963615637081:web:257219774783eeed6c7b48"
+  }
+};
