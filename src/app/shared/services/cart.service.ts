d5b3803ee08ee8758c70caaaccfdf934e6c30131
Paul DAO <dao.van.paul@gmail.com>
2023-11-21 15:10:57 -0500

    UPDATE cart service write add

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable }                  from '@angular/core';
+import { BehaviorSubject, Observable } from "rxjs";
+import { Cart, CartItem }              from "../interfaces/cart.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  add(id: string): void {
-    console.log('add ', id)
+  cart$: BehaviorSubject<Cart> = new BehaviorSubject<Cart>({ items: [] });
+
+  getCart$(): Observable<Cart | undefined> {
+    return this.cart$.asObservable();
+  }
+
+  add(item: CartItem): void {
+    console.log('add ', item);
+    const items: CartItem[] = [...this.cart$.value.items];
+
+    const itemInCart: CartItem | undefined = items.find(_item => _item.id === item.id);
+    if (itemInCart) {
+      itemInCart.quantity += 1;
+    } else {
+      items.push(item);
+    }
+
+    this.cart$.next({ items });
   }
 
   remove(): void {
