6ca5368cfe8bf583ea8846962b33cda46dd17ae0
Paul DAO <dao.van.paul@gmail.com>
2023-11-21 15:09:08 -0500

    UPDATE card to buy interface add price

--- src/app/shared/interfaces/card-to-buy.interface.ts
+++ src/app/shared/interfaces/card-to-buy.interface.ts
@@ -2,6 +2,7 @@ import { CardNFT } from "./card-nft.interface";
 
 export interface CardToBuy extends CardNFT {
   idListing: number,
+  price: number,
   lastSale?: {
     price: number,
     currency: string
