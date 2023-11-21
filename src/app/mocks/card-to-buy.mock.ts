e0206afe8cbaefe98b780dc211b9ed03d4ab970c
Paul DAO <dao.van.paul@gmail.com>
2023-11-21 15:08:57 -0500

    UPDATE card to buy mock

--- src/app/mocks/card-to-buy.mock.ts
+++ src/app/mocks/card-to-buy.mock.ts
@@ -2,6 +2,7 @@ import { CardToBuy } from '../shared/interfaces';
 
 export const cardToBuyMock: CardToBuy = {
   id: '1231',
+  nameSeo: 'name',
   idListing: 332,
   title: 'Chaos Road',
   price: 0.99,
