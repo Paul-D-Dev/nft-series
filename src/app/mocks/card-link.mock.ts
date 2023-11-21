feb38a4f3d92b7c6fd50285f308c335e62016d75
Paul DAO <dao.van.paul@gmail.com>
2023-11-21 15:08:41 -0500

    UPDATE card link mock

--- src/app/mocks/card-link.mock.ts
+++ src/app/mocks/card-link.mock.ts
@@ -2,51 +2,69 @@ import { CardLink } from '../shared/interfaces';
 
 export const cardLinkMock: CardLink[] = [
   {
-    id: 1,
+    id: '1',
     nameSeo: 'stranger-things',
     title: 'Stranger Things',
     floor: 0.09,
-    devise: 'ETH',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
   {
-    id: 2,
+    id: '2',
     nameSeo: 'stranger-things-1',
     title: 'Stranger Things 2',
     floor: 1.35,
-    devise: 'BSC',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
   {
-    id: 3,
+    id: '3',
     nameSeo: 'stranger-things-2',
     title: 'Stranger Things 3',
     floor: 10.59,
-    devise: 'POL',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
   {
-    id: 4,
+    id: '4',
     nameSeo: 'stranger-things-3',
     title: 'Stranger Things',
     floor: 0.09,
-    devise: 'ETH',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
   {
-    id: 5,
+    id: '5',
     nameSeo: 'stranger-things-4',
     title: 'Stranger Things 2',
     floor: 1.35,
-    devise: 'BSC',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
   {
-    id: 6,
+    id: '6',
     nameSeo: 'stranger-things-5',
     title: 'Stranger Things 3',
     floor: 10.59,
-    devise: 'POL',
-    imgUrl: 'https://picsum.photos/600'
+    currency: 'ETH',
+    img: {
+      src: 'https://picsum.photos/600',
+      alt: 'https://picsum.photos/600'
+    },
   },
 ];
