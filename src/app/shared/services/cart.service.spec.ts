f43fceb44280b2ebb0b4b656072925ace742b911
Paul DAO <dao.van.paul@gmail.com>
2023-11-21 16:09:40 -0500

    TEST cart service implementation

--- src/app/shared/services/cart.service.spec.ts
+++ src/app/shared/services/cart.service.spec.ts
@@ -1,10 +1,37 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed }     from '@angular/core/testing';
 import { CartService } from './cart.service';
+import { CardNFT }     from "../interfaces";
 
-describe('CartService', () => {
+fdescribe('CartService', () => {
   let service: CartService;
-
+  const items: CardNFT[] = [
+    {
+      id: '1',
+      price: 2,
+      creator: 'zou',
+      currency: 'ETH',
+      title: 'Mystery',
+      nameSeo: 'zou-mystery',
+      img: {
+        src: 'sourceImage',
+        alt: 'altImage'
+      },
+      idListing: '3'
+    },
+    {
+      id: '2',
+      price: 3,
+      creator: 'zoulou',
+      currency: 'POL',
+      title: 'Making',
+      nameSeo: 'zoulou-making',
+      img: {
+        src: 'sourceImage',
+        alt: 'altImage'
+      },
+      idListing: '1'
+    },
+  ]
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CartService);
@@ -12,5 +39,39 @@ describe('CartService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add item to the cart', () => {
+    service.add(items[0]);
+    service.getCart$().subscribe(cart => {
+      expect(cart?.items.length).toBe(1);
+      expect(cart?.items[0]).toBe(items[0]);
+    });
+  });
+
+  it('should not add duplicate item to the cart', () => {
+    service.add(items[0]);
+    service.add(items[0]);
+    service.getCart$().subscribe(cart => {
+      expect(cart?.items.length).toBe(1);
+    });
+  });
+
+  it('should remove item from the cart', () => {
+    service.add(items[0]);
+    service.remove(items[0]);
+    service.getCart$().subscribe(cart => {
+      expect(cart?.items.length).toBe(0);
+    });
+  });
+
+  it('should clear all items from the cart', () => {
+    service.add(items[0]);
+    service.add(items[1]);
+    service.clearAll();
+
+    service.getCart$().subscribe(cart => {
+      expect(cart?.items.length).toBe(0);
+    });
   });
 });
