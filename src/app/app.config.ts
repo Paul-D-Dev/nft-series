64220b78c5ecc49e5515e397543c85162572e061
Paul DAO <dao.van.paul@gmail.com>
2023-11-22 13:45:15 -0500

    PACKAGE install ngrx store and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,17 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
-    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
-
-  ]
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    provideStore(),
+    provideEffects()
+]
 };
 
 register();
