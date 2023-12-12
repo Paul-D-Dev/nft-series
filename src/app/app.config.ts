b6739fb6a64067d9a0ab5c3faf7a13d4ce7e4344
Paul DAO <dao.van.paul@gmail.com>
2023-12-12 13:02:45 -0500

    FEAT app config init firebase app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
-    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
-
-  ]
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
+    ])
+  ],
 };
 
 register();
